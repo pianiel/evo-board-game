0a17f10051fff440b72b157ed4f72aa8527d028e
Piotrek <pianiel@example.com>
2014-02-11 22:57:12 +0100

    comments

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from src.model import *
 import src.gui
 
 from src.model.evolution.evolution import main, arena, humanarena, monkeyrun, experiments
-# from src.gui.tkinter_demo import main
 
 '''
 params_names = [
@@ -16,18 +15,21 @@ params_names = [
 ]
 '''
 
+# list of parameter sets
 params = [
     (15, 30,  0.2, 1.5, 10, 2, 5),
     ( 5, 50,  0.2, 1.5, 10, 2, 5),
     (10, 40, 0.15, 1.2, 10, 2, 5),
     (10, 40,  0.3, 1.8, 10, 2, 5)
 ]
+
+# how many random genotypes
 n_randoms = 200
 
 
 if __name__ == '__main__':
-	# main()
-	# arena()
-	# humanarena()
-	# monkeyrun(50)
-	experiments(params, n_randoms)
+	# run experiments
+	# experiments(params, n_randoms)
+	
+	# in order to play as a human
+	humanarena(agent_sol=[1.0,-0.5,0.8,-0.5,0.1,0.2])
